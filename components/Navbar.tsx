0a77941c8722c30afe46189899c7965b7b0b4104
HarshaFdo <fernando.harsha2016@gmail.com>
2025-06-02 10:38:50 +0530

    ipmement clerck auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,13 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import NavItems from "./NavItems";
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>
+    <nav className="navbar">
       <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
           <Image src="/images/logo.png" alt="logo" width={46} height={44} />
@@ -14,7 +15,14 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign In</p>
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
