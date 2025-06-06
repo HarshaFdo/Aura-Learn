8cec5cbbb53dc76757cfe5382b391ec93e1b8fd2
HarshaFdo <fernando.harsha2016@gmail.com>
2025-06-06 10:38:29 +0530

    transcript modifications

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -105,7 +105,7 @@ const CompanionComponent = ({
   };
 
   return (
-    <section className="flex flex-col h-[70vh]">
+    <section className="flex flex-col h-screen">
       <section className="flex gap-8 max-sm:flex-col">
         <div className="companion-section">
           <div
