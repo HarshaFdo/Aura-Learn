c5927b302448c80cafb33469733d27c0b5a9c6ac
HarshaFdo <fernando.harsha2016@gmail.com>
2025-06-07 16:55:12 +0530

    Final

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -141,3 +141,16 @@ export const newCompanionPermissions = async () => {
     return true;
   }
 };
+
+export const getBookmarkedCompanions = async (userId: string) => {
+  const supabase = createsupabaseClient();
+  const { data, error } = await supabase
+    .from("bookmarks")
+    .select(`companions:companion_id (*)`) // Notice the (*) to get all the companion data
+    .eq("user_id", userId);
+  if (error) {
+    throw new Error(error.message);
+  }
+  // We don't need the bookmarks data, so we return only the companions
+  return data.map(({ companions }) => companions);
+};
